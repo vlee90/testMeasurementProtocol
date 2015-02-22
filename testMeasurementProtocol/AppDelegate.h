2209dd0f522fe26bb42fb5f922ca1eaa0263b6b0
Vincent Lee <lee.m.vincent@gmail.com>
2015-02-21 16:55:10 -0800

    Initial Commit

---
+++ testMeasurementProtocol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testMeasurementProtocol
+//
+//  Created by Vincent Lee on 2/21/15.
+//  Copyright (c) 2015 VincentLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testMeasurementProtocol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testMeasurementProtocol
+//
+//  Created by Vincent Lee on 2/21/15.
+//  Copyright (c) 2015 VincentLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
